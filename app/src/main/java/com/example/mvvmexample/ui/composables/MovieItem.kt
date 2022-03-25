363cd5efd26156b28e849cb75da32038682b8f52
rperezsolis <irafael.perez826@gmail.com>
2022-03-25 14:40:54 -0600

    Implemented coil for loading movies' pictures.

---
+++ app/src/main/java/com/example/mvvmexample/ui/composables/MovieItem.kt
@@ -0,0 +1,52 @@
+package com.example.mvvmexample.ui.composables
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.mvvmexample.R
+import com.example.mvvmexample.model.Movie
+
+@Composable
+fun MovieItem(
+    movie: Movie,
+    modifier: Modifier = Modifier
+) {
+    val posterUrl = "https://image.tmdb.org/t/p/w185${movie.posterUrl}"
+
+    Card(
+        elevation = 4.dp,
+        shape = RoundedCornerShape(16.dp),
+        modifier = modifier
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(all = 16.dp)
+        ) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(posterUrl)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = stringResource(R.string.text_picture_poster),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            Text(
+                text = movie.title,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/mvvmexample/ui/composables/MovieScreen.kt
+++ app/src/main/java/com/example/mvvmexample/ui/composables/MovieScreen.kt
@@ -1,16 +1,20 @@
 package com.example.mvvmexample.ui.composables
 
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.mvvmexample.model.Movie
 import com.example.mvvmexample.viewmodel.MoviesViewModel
 import org.koin.androidx.compose.getViewModel
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MoviesScreen(
     modifier: Modifier = Modifier
@@ -19,11 +23,15 @@ fun MoviesScreen(
     val movieViewModel: MoviesViewModel = getViewModel()
     val movieList: List<Movie> by movieViewModel.movies.collectAsState()
 
-    LazyColumn(
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(2),
         modifier = modifier
     ) {
         items(movieList) { movie ->
-            Text(text = movie.title)
+            MovieItem(
+                movie = movie,
+                modifier = Modifier.padding(all = 12.dp)
+            )
         }
     }
 }
